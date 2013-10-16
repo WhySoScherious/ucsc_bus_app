112e23866f373e9089bba666c73013ab2108d6c3
wakerider47 <coles.shit@gmail.com>
2013-10-16 15:07:34 -0700

    added custom "normal" buttons. need to create .9.png's for mybutton_pressed, mybutton_selected, and mybutton_disabled. Also need to changed button background from white to transparent w/ photoshop.

--- UCSCBusBuddy/gen/com/example/ucscbusbuddy/R.java
+++ UCSCBusBuddy/gen/com/example/ucscbusbuddy/R.java
@@ -22,12 +22,15 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int mybutton=0x7f020001;
+        public static final int mybutton_normal=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int bus_schedule=0x7f080001;
+        public static final int action_settings=0x7f080004;
+        public static final int bus_schedule=0x7f080000;
         public static final int closest_stop=0x7f080002;
-        public static final int select_stop=0x7f080000;
+        public static final int select_stop=0x7f080001;
+        public static final int textView1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
