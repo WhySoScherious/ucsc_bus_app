dcc26041d340b5978b16b79eb5ea4d1e346be3cc
wakerider47 <coles.shit@gmail.com>
2013-11-02 15:08:45 -0700

    removed more commented out code

--- UCSCBusBuddy/src/com/example/ucscbusbuddy/ClosestStopActivity.java
+++ UCSCBusBuddy/src/com/example/ucscbusbuddy/ClosestStopActivity.java
@@ -51,23 +51,5 @@ public class ClosestStopActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-/* =======
-
-public class ClosestStopActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_closest_stop);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.closest_stop, menu);
-		return true;
->>>>>>> refs/heads/cole
-
-*/
 	}
 }
--- UCSCBusBuddy/src/com/example/ucscbusbuddy/MainActivity.java
+++ UCSCBusBuddy/src/com/example/ucscbusbuddy/MainActivity.java
@@ -29,36 +29,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-/*
-<<<<<<< HEAD
-    private void setButtonClickListener(int idButton) {
-        Button toggleButton = (Button)findViewById(idButton);
-        toggleButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (v.getId() == R.id.bus_schedule)
-                    busSchedule (v);
-                else if (v.getId() == R.id.closest_stop)
-                    closestStop (v);
-                else
-                    selectStop (v);
-            }
-        });
-=======
-    public void busSchedule( View view ){
-    	Intent intent = new Intent( this, BusScheduleActivity.class );
-    	startActivity( intent );
-    }
-    
-    public void selectStop( View view ){
-    	Intent intent = new Intent( this, SelectStopActivity.class );
-    	startActivity( intent );
-    }
-    
-    public void closestStop( View view ){
-    	Intent intent = new Intent( this, ClosestStopActivity.class );
-    	startActivity( intent );
->>>>>>> refs/heads/cole
-    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
